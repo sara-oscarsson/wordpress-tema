bb74beb9280bdcf90e7e79ba8da4e9fc5690be4d
Sara Oscarsson <oscarsson.sara@hotmail.com>
2021-04-13 09:54:03 +0200

    header-footer-link-to-style-and-js

--- wp-content/themes/labb1/index.php
+++ wp-content/themes/labb1/index.php
@@ -1,1 +1,26 @@
-banan
+<?php
+ get_header();
+?>
+	<section>
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="hero">
+                            <?php
+                            while( have_posts()){
+                                the_post();
+                                the_content();
+                            }
+                            ?>
+								<!-- <img src="img/city.jpg" />
+								<div class="text">
+									
+								</div> -->
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+<?php
+ get_footer();
+?>
